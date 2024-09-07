66f58302c9cef3ff66b03a457f5b67b883e244cf
junu-23 <wjw5892@gmail.com>
2024-09-07 16:55:42 +0900

    Update forms.py

--- pybo1/forms.py
+++ pybo1/forms.py
@@ -6,7 +6,7 @@ class QuestionForm(forms.ModelForm): #질문 등록시 사용할 Question폼
         model = Question #사용할 모델
         fields = ['subject', 'content'] # QuestionForm에서 사용할 Question 모델의 속성
         #모델 폼은 모델과 연결된 폼으로, 폼을 저장하면 연결된 모델의 데이터 저장 가능
-        labels = { #레이블 쓰는 이유?
+        labels = { #레이블?
             'subject': '제목',
             'content': '내용',
         }
